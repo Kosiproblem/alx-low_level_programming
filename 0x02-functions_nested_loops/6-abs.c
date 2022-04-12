dc008a6ac6a7dfdba62172d706fbb8bbfdb817de
kosiProblem <kosyarnold@gmail.com>
2022-04-12 08:26:19 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
+
+}
