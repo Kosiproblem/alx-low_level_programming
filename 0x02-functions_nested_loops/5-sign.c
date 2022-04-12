dae17d7e5f7746bf0711acb7e211299cd89fd2f7
kosiProblem <kosyarnold@gmail.com>
2022-04-12 10:38:44 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- * main - Entry point
- *
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ * @n: the int to print
  * Return: Always 0.
  */
 int print_sign(int c)
