17e456d38cb22f49b569525af5b19ccff8185f7b
kosiProblem <kosyarnold@gmail.com>
2022-04-14 08:36:40 -0700

    fix bugs

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 /**
  * main - checks for uppercase character
  * @c: character to check case
- * Return:0 or 1
+ * Return: 0 or 1 if c is uppercase
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'));
-                return (1);
+	if((c >= 'A') && (c <= 'Z'))
+		return (1);
 	else
 		return (0);
 }
